68f7d14a1703fe43ba732731cc5c01e6d319244b
khaityy <khaionhulu@gmail.com>
2024-04-04 12:14:12 -0500

    Feeling more comfortable, yay finally, adding the select function into the mix and then moving to displaying the Question after click

--- app.js
+++ app.js
@@ -54,6 +54,21 @@ const categories = [
   }
 ];
 
+function selectQuestions(categories) {
+  const selectedQuestions = [];
+  
+}
+
+
+
+
+
+
+
+
+
+
+
 function displayQuestion(question) {
   const questionContainer = document.getElementById('')
 }
@@ -79,13 +94,12 @@ function displayQuestion(question) {
 
 
 
-// const questions = ['Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5', 'Question 6', 'Question 7', 'Question 8', 'Question 9', 'Question 10', 'Question 11', 'Question 12', 'Question 13', 'Question 14', 'Question 15', 'Question 16', 'Question 17','Question 18', 'Question 19', 'Question 20', 'Question 21', 'Question 22', 'Question 23', 'Question 24', 'Question 25']
+// 
 
 
 
 //    VARIABLES
-let player = user1
-let Cat1, Ques1 = 'sqr0'
+
 
 
 
