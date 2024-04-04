3693f4dc49e0052aa2846ed90482b4c935a3cc74
khaityy <khaionhulu@gmail.com>
2024-04-04 11:13:15 -0500

    Putting questions into proper format to use the prices to interect

--- app.js
+++ app.js
@@ -2,8 +2,41 @@
 const user1 = "placeholder"
 const submitBtn = document.querySelector('button')
 const squareEls = document.querySelector("sqr")
-const categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5']
-const questions = ['Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5', 'Question 6', 'Question 7', 'Question 8', 'Question 9', 'Question 10', 'Question 11', 'Question 12', 'Question 13', 'Question 14', 'Question 15', 'Question 16', 'Question 17','Question 18', 'Question 19', 'Question 20', 'Question 21', 'Question 22', 'Question 23', 'Question 24', 'Question 25']
+// const categories = [
+{ name: 'American Idol Fandom,
+  questions: [
+    { question: "Who is the first American Idol contestant to win the show?' },
+    { question: "Who is the judge that has been apart of the most seasons for American Idol?"},
+    { question: ''}
+
+
+
+  ]
+
+
+
+}
+
+]
+
+
+
+
+['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5']
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const questions = ['Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5', 'Question 6', 'Question 7', 'Question 8', 'Question 9', 'Question 10', 'Question 11', 'Question 12', 'Question 13', 'Question 14', 'Question 15', 'Question 16', 'Question 17','Question 18', 'Question 19', 'Question 20', 'Question 21', 'Question 22', 'Question 23', 'Question 24', 'Question 25']
 
 
 
@@ -44,6 +77,7 @@ function generateBoard() {
   }
 
   function displayQuestion()
+
 
 
 
