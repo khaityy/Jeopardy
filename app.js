4078ba82820e688f877d524659ee54bf27092d18
khaityy <khaionhulu@gmail.com>
2024-04-04 07:30:42 -0500

    Adding functionality to the board

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const user1 = "placeholder"
 const submitBtn = document.querySelector('button')
 const squareEls = document.querySelector("sqr")
 const categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5']
-const 
+const questions = ['Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5', 'Question 6', 'Question 7', 'Question 8', 'Question 9', 'Question 10', 'Question 11', 'Question 12', 'Question 13', 'Question 14', 'Question 15', 'Question 16', 'Question 17','Question 18', 'Question 19', 'Question 20', 'Question 21', 'Question 22', 'Question 23', 'Question 24', 'Question 25']
+
+
 
 //    VARIABLES
 let player = user1
@@ -16,7 +18,16 @@ submitBtn.addEventListener('click', handleClick)
 
 
 // INIT AND RENDER FUCNTIONS
+function generateBoard() {
+  const gameBoard = document.getElementById('game-board')
+  for (let i )
 
+
+
+
+
+
+}
 
 
 
