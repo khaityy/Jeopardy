1177be3454e1cfe9942f715d170288273f3dad54
khaityy <khaionhulu@gmail.com>
2024-04-04 07:18:13 -0500

    add prices

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 const user1 = "placeholder"
 const submitBtn = document.querySelector('button')
 const squareEls = document.querySelector("sqr")
-
+const
 
 //    VARIABLES
 let player = user1
-
+let squareEls
 
 
 
 //    EVENT LISTENER
-submitBtn.addEventListener('click', init)
+submitBtn.addEventListener('click', handleClick)
 
 
 // INIT AND RENDER FUCNTIONS
